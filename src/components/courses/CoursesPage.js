cac36aff0926bf2b32a43d24c888fde6a491ee37
YaseminGokberk <yasemingokberk@gmail.com>
2020-01-05 18:56:04 +0300

    bindingActionCreator was used

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,64 +1,59 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect} from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
-import PropTypes from "prop-types";
+import propTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
-class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
+class CoursesPage extends React.Component{ 
+        state = {
+            course:{
+                title: ""
+            }
+        }      
+    
+    handleChange = event => {
+        const course = {...this.state.course, title: event.target.value};
+        this.setState({ course });
     }
-  };
+    handleSubmit  = event => {        
+        event.preventDefault();        
+        this.props.actions.createCourse(this.state.course);
+       
+    }
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title}/>
+                <input type="submit" value="Save"/>
 
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-
-        <input type="submit" value="Save" />
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
-    );
-  }
+                {this.props.courses.map(course => (
+                    <div key={course.title}>{course.title}</div>
+                ))}
+            </form>
+        );
+    }
 }
 
 CoursesPage.propTypes = {
-  courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-};
-
-function mapStateToProps(state) {
-  return {
-    courses: state.courses
-  };
+    courses : propTypes.array.isRequired,
+    actions: propTypes.object.isRequired
+}
+//What state is pass to our component via props
+function mapStateToProps(state){    
+    return{
+        courses : state.courses
+    };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseActions, dispatch)
-  };
+function mapDispatchToProps(dispatch){
+    return{
+        actions: bindActionCreators(courseActions,dispatch)
+    };
 }
+// declare what actions to pass to our component on props 
+//function mapDispatchToProps()
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CoursesPage);
+//connect returns a function and then calls our component
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
